a33bb9c159a48bf3160db9e01cdd1affddee0784
vamshivr5776 <vamshivr5776@gmail.com>
2026-08-22 13:06:05 +0530

    chore: initialize Dayflow project structure

---
+++ frontend/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "YOUR_SUPABASE_URL";
+const SUPABASE_PUBLISHABLE_KEY = "YOUR_PUBLISHABLE_KEY";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
